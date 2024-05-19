c4878d4c53b0e18410ee9347e319b0a4e3e59d9c
bluejazx <108899308+bluejazx@users.noreply.github.com>
2024-05-19 12:33:24 -0400

    Create BlockLetters.cpp

---
+++ BlockLetters.cpp
@@ -0,0 +1,14 @@
+//This program prints Block Letters to the console
+#include <iostream>
+
+int main()
+{
+	//Prints the initials SL
+	std::cout << " SSS ";
+	std::cout << "S   S";
+	std::cout << "S";
+	std::cout << " SSS";
+	std::cout << "    S";
+	std::cout << "S   S";
+	std::cout << " SSS";
+}
